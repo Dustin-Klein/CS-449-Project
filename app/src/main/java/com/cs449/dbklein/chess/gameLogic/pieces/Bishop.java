04589bfe5cb09b1c18180eec942e6ef038de98e5
Dustin Klein <dbklein4@gmail.com>
2018-12-01 16:56:42 -0600

    Added ability for bishop to move.

--- app/src/main/java/com/cs449/dbklein/chess/gameLogic/pieces/Bishop.java
+++ app/src/main/java/com/cs449/dbklein/chess/gameLogic/pieces/Bishop.java
@@ -1,6 +1,7 @@
 package com.cs449.dbklein.chess.gameLogic.pieces;
 
 import android.content.res.Resources;
+import android.util.Pair;
 
 import com.cs449.dbklein.chess.R;
 import com.cs449.dbklein.chess.gameLogic.Board;
@@ -32,29 +33,65 @@ public class Bishop extends Piece {
             throw new IllegalStateException("Piece not matching up at cells");
 
         ArrayList<Move> validMoves = new ArrayList<>();
+        ArrayList<Pair> possibleDest = new ArrayList<>();
 
-        // Up and Left
-//        for (int i = Math.min(sourceRow, sourceCol); i >= 0; i--) {
-//            System.out.println((sourceRow - i) + "," + (sourceCol - i));
-//        }
-        // Up and Right
-//        for (int i = Math.min(sourceRow, 8 - sourceCol); i >= 0; i--) {
-//            System.out.println((sourceRow - i) + "," + (sourceCol + i));
-//        }
+        for (int dx : new int[]{-1, 1})
+            for (int dy : new int[]{-1, 1})
+                possibleDest.addAll(getPossibleDestinations(sourceRow, sourceCol, dx, dy));
 
-        for (int i = 0; i <= 7; i++) {
-            for (int j = 0; j <= 7; j++) {
-                if (isValidDestination(sourceRow, sourceCol, i, j, board)) {
-                    validMoves.add(new Move(sourceRow, sourceCol, i, j, this));
-                }
-            }
+
+        for (Pair dest : possibleDest) {
+            if (isValidDestination(sourceRow, sourceCol, (int) dest.first, (int) dest.second, board))
+                validMoves.add(new Move(sourceRow, sourceCol, (int) dest.first, (int) dest.second, this));
         }
-        System.out.println(validMoves);
 
         return validMoves;
     }
 
     private boolean isValidDestination(int sourceRow, int sourceCol, int destRow, int destCol, Board board) {
-        return board.inBounds(destRow, destCol);
+        if (!board.inBounds(destRow, destCol))
+            return false;
+
+        if (board.getCell(destRow, destCol).getPiece().getColor() == color)
+            return false;
+
+        if (sourceRow == destRow && sourceCol == destCol)
+            return false;
+
+        int dr = sourceRow < destRow ? 1 : -1;
+        int dc = sourceCol < destCol ? 1 : -1;
+
+        // Check every cell in between the source and destination
+        int col = sourceCol + dc;
+        for (int row = sourceRow + dr; row != destRow; row += dr) {
+
+            if (board.getCell(row, col).isOccupied())
+                return false;
+
+            col += dc;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns a list of all possible destinations on the diagonals of the bishop.
+     * This does not verify that every one of these destinations are valid.
+     */
+    private ArrayList<Pair> getPossibleDestinations(int sourceRow, int sourceCol, int dr, int dc) {
+        ArrayList<Pair> possibleDest = new ArrayList<>();
+
+        for (int i = 1; i < 8; i++) {
+
+            int destRow = sourceRow + dr * i;
+            int destCol = sourceCol + dc * i;
+
+            if (0 <= destRow && destRow < 8 && 0 <= destCol && destCol < 8)
+                possibleDest.add(new Pair<>(destRow, destCol));
+            else
+                break;
+        }
+
+        return possibleDest;
     }
 }
